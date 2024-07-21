057697fffad6ce358bb513431188bf7ab0ff579f
AlabasterRig <utkrist.j@gmail.com>
2024-07-22 03:36:51 +0530

    Code Addition

--- Engine3D/Source/Engine3D.cpp
+++ Engine3D/Source/Engine3D.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 struct vec3d
 {
-    float x, y, z;
+	float x, y, z;
 };
 
 
 struct triangle
 {
-    vec3d p[3];
+	vec3d p[3];
 };
 
 
 struct mesh
 {
-    vector<triangle> tris;
+	vector<triangle> tris;
 };
 
 struct mat4x4
 {
-    float m[4][4] = { 0 };
+	float m[4][4] = { 0 };
 };
 
 
@@ -28,127 +28,129 @@ class GameEngine3D : public olcConsoleGameEngine
 {
 
 public:
-    GameEngine3D()
-    {
-        m_sAppName = L"3D Demo";
-    }
+	GameEngine3D()
+	{
+		m_sAppName = L"3D Demo";
+	}
 
 private:
-    mesh meshCube;
-    mat4x4 matProj;
+	mesh meshCube;
+	mat4x4 matProj;
 
-    void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m) // i = input, o = output, m = matrix
-    {
+	void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m) // i = input, o = output, m = matrix
+	{
 
-        o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
-        o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
-        o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
-        float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+		o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+		o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+		o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+		float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
 
-        if (w != 0.0f)
-        {
-            o.x /= w; o.y /= w; o.z /= w;
-        }
-    }
+		if (w != 0.0f)
+		{
+			o.x /= w; o.y /= w; o.z /= w;
+		}
+	}
 
 public:
 
-    bool OnUserCreate() override
-    {
-        meshCube.tris = {
+	bool OnUserCreate() override
+	{
+		meshCube.tris = {
 
-            // SOUTH
-            { 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
-            { 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+			// SOUTH
+			{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 
-            // EAST                                                      
-            { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
-            { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+			// EAST                                                      
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
 
-            // NORTH                                                     
-            { 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
-            { 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+			// NORTH                                                     
+			{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
 
-            // WEST                                                      
-            { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
-            { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+			// WEST                                                      
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
 
-             // TOP                                                       
-             { 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
-             { 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+			// TOP                                                       
+			{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+			{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
 
-             // BOTTOM                                                    
-             { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
-             { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+			// BOTTOM                                                    
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 
-        };
+		};
 
-        //Projection Matrix
-        float fNear = 0.1f;
-        float fFar = 1000.0f;
-        float fFov = 90.0f;
-        float aspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
-        float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+		//Projection Matrix
+		float fNear = 0.1f;
+		float fFar = 1000.0f;
+		float fFov = 90.0f;
+		float aspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
 
-        matProj.m[0][0] = aspectRatio * fFovRad;
-        matProj.m[1][1] = fFovRad;
-        matProj.m[2][2] = fFar / (fFar - fNear);
-        matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
-        matProj.m[2][3] = 1.0f;
-        matProj.m[3][3] = 0.0f;
+		matProj.m[0][0] = aspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+		matProj.m[2][3] = 1.0f;
+		matProj.m[3][3] = 0.0f;
 
-        return true;
-    }
+		return true;
+	}
 
-    bool OnUserUpdate(float fElapsedTime)
-    {
+	bool OnUserUpdate(float fElapsedTime) override
+	{
 
-        Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
 
-        //Draw Triangles
-        for (auto tri : meshCube.tris)
-        {
-            triangle triProjected, triTranslated;
+		//Draw Triangles
+		for (auto tri : meshCube.tris)
+		{
+			triangle triProjected, triTranslated;
 
-            triTranslated = tri;
-            triTranslated.p[0].z = tri.p[0].z + 3.0f;
-            triTranslated.p[1].z = tri.p[1].z + 3.0f;
-            triTranslated.p[2].z = tri.p[2].z + 3.0f;
+			triTranslated = tri;
+			triTranslated.p[0].z = tri.p[0].z + 3.0f;
+			triTranslated.p[1].z = tri.p[1].z + 3.0f;
+			triTranslated.p[2].z = tri.p[2].z + 3.0f;
 
 
-            MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
-            MultiplyMatrixVector(tri.p[1], triProjected.p[1], matProj);
-            MultiplyMatrixVector(tri.p[2], triProjected.p[2], matProj);
+			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
 
-            //Scale into view
-            triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
-            triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
-            triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
+			//Scale into view
+			triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
+			triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
+			triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
 
-            triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
-            triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
-            triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
-            triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
-            triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
-            triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
+			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
+			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
+			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
 
-            DrawTriangle(triProjected.p[0].x, triProjected.p[0].y, triProjected.p[1].x,
-                triProjected.p[1].y, triProjected.p[2].x, triProjected.p[2].y);
-        }
+			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y, triProjected.p[1].x,
+				triProjected.p[1].y, triProjected.p[2].x, triProjected.p[2].y, PIXEL_SOLID, FG_WHITE);
+		}
 
-        return true;
-    }
+		return true;
+	}
 };
 
 
 
 int main()
 {
-    GameEngine3D demo;
+	GameEngine3D demo;
 
-    if (demo.ConstructConsole(256, 240, 4, 4))
-        demo.Start();
+	if (demo.ConstructConsole(120, 90, 1, 1)) 
+	{
+		demo.Start();
+	}
 
-    return 0;
+	return 0;
 }
 
